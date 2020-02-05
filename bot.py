266e84df873e4a6d2d7e547fa5d20405d5d90fa8
ethansky <fragglekids@gmail.com>
2020-02-04 23:40:12 -0500

    json parsing for IDs and tokens

--- bot.py
+++ bot.py
@@ -3,16 +3,21 @@ from __future__ import print_function
 
 import os.path
 import pickle
-
+import json
 import discord
 from discord.ext import commands
 from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 
+
+with open('ids.json') as json_file:
+    data = json.load(json_file)
+
 SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
-SPREADSHEET_ID = ''
-RANGE_NAME = ''
+SPREADSHEET_ID = data['spreadsheetID']
+RANGE_NAME = data['rangeName']
+BOT_TOKEN = data['botToken']
 
 # init Google Spreadsheets API
 
@@ -32,8 +37,6 @@ service = build('sheets', 'v4', credentials=creds)
 sheet = service.spreadsheets()
 
 # init Discord bot API
-with open('bottoken.txt', 'r') as tokenfile:
-    btoken = tokenfile.read()
 bot = commands.Bot(command_prefix='>')
 
 bot.run(btoken)
